1d1cc2b6dbe8eeebd5122c64c248da9981951440
olehall66 <olehall66@gmail.com>
2015-07-27 10:32:51 -0700

    monday commit

---
+++ js/bikazondata.js
@@ -0,0 +1,481 @@
+// Data file for Sam Racina images
+// 7-20-15
+
+// Wall One
+var Castles={
+  imgLoc:"Castles",
+  imgSrc:"images/thecoastranges_wall1/castles.jpg",
+  imgTitle:"The Castles",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'10"x8" inches',
+  imgPrice:"$995.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var LateAlamedaCo={
+  imgLoc:"LateAlamedaCo",
+  imgSrc:"images/thecoastranges_wall1/latealamedaco.jpg",
+  imgTitle:"Late Afternoon in Southeast Alameda County",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'24"x48" inches',
+  imgPrice:"$8,900.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var NortheastSonomacountyOakRidgeRanch={
+  imgLoc:"NortheastSonomacountyOakRidgeRanch",
+  imgSrc:"images/thecoastranges_wall1/northeastsonomaco_oakridgeranch.jpg",
+  imgTitle:"Northeast Sonoma County (Oak Ridge Ranch)",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'22"x48" inches',
+  imgPrice:"$8,500.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var OakVinesHills={
+  imgLoc:"OakVinesHills",
+  imgSrc:"images/thecoastranges_wall1/oakvineshills.jpg",
+  imgTitle:"Oaks, Vines and Hills",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'10"x15" inches',
+  imgPrice:"$1,600.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var OnBolinasRidge={
+  imgLoc:"OnBolinasRidge",
+  imgSrc:"images/thecoastranges_wall1/onbolinasridge.jpg",
+  imgTitle:"On Bolinas Ridge",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'32"x48" inches',
+  imgPrice:"$9,500.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var RedwoodMountain={
+  imgLoc:"RedwoodMountain",
+  imgSrc:"images/thecoastranges_wall1/redwoodmt4.jpg",
+  imgTitle:"Redwood Mountain",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'32"x48" inches',
+  imgPrice:"$12,500.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var SandDollarBeach={
+  imgLoc:"SandDollarBeach",
+  imgSrc:"images/thecoastranges_wall1/sandollarbeach.jpg",
+  imgTitle:"Sand Dollar Beach",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'30"x48" inches',
+  imgPrice:"$9,800.",
+  origIsAvail:true,
+  imgPrtPrice:"$1,400.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var ThompsonRidge={
+  imgLoc:"ThompsonRidge",
+  imgSrc:"images/thecoastranges_wall1/thompsonridge.jpg",
+  imgTitle:"Thompson Ridge",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'36"x48" inches',
+  imgPrice:"$7,800.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var wall_1=[Castles,LateAlamedaCo,NortheastSonomacountyOakRidgeRanch,OakVinesHills,OnBolinasRidge,RedwoodMountain,SandDollarBeach,ThompsonRidge]
+//end wall One
+
+// Wall Two
+var OaksAndWildflowers={
+  imgLoc:"OaksAndWildflowers",
+  imgSrc:"images/mostlyspring_wall2/framedoakwildflowers.jpg",
+  imgTitle:"Oaks and Wildflowers",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'32"x48" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$1,400.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var VentanaSpring={
+  imgLoc:"VentanaSpring",
+  imgSrc:"images/mostlyspring_wall2/ventanaspring.jpg",
+  imgTitle:"Ventana Spring",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'28"x40" inches',
+  imgPrice:"$6,700.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var PruneOrchard={
+  imgLoc:"PruneOrchard",
+  imgSrc:"images/mostlyspring_wall2/printpruneorc.jpg",
+  imgTitle:"Prune Orchard",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'26"x48" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$1,400.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var OakInFog={
+  imgLoc:"OakInFog",
+  imgSrc:"images/mostlyspring_wall2/poakinfog.jpg",
+  imgTitle:"Valley Oak in Fog",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'32"x48" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$1,400.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var DodgeTrucks={
+  imgLoc:"DodgeTrucks",
+  imgSrc:"images/mostlyspring_wall2/pdodgetrk.jpg",
+  imgTitle:"Dodge Trucks and Woodpile",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'16"x24" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$800.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var InlandCentralCalifornia={
+  imgLoc:"InlandCentralCalifornia",
+  imgSrc:"images/mostlyspring_wall2/innercentralcoast.jpg",
+  imgTitle:"Inland Central California",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'18"x24" inches',
+  imgPrice:"$2,100.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var Spring={
+  imgLoc:"Spring",
+  imgSrc:"images/mostlyspring_wall2/finalspring.jpg",
+  imgTitle:"Spring",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'32"x48" inches',
+  imgPrice:"$9,800.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var RailroadBridge={
+  imgLoc:"RailroadBridge",
+  imgSrc:"images/mostlyspring_wall2/railroadbridge.jpg",
+  imgTitle:"Railroad Bridge",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'16"x24" inches',
+  imgPrice:"$5,500.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var wall_2=[OaksAndWildflowers,VentanaSpring,PruneOrchard,OakInFog,DodgeTrucks,InlandCentralCalifornia,Spring,RailroadBridge];
+// end Wall two
+
+// Wall Three
+var AlabamaHills={
+  imgLoc:"AlabamaHills",
+  imgSrc:"images/thedesertandgreatbasin_wall3/Alabamahills.jpg",
+  imgTitle:"Alabama Hills",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'20"x16" inches',
+  imgPrice:"$3,500.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var GrandCanyon={
+  imgLoc:"GrandCanyon",
+  imgSrc:"images/thedesertandgreatbasin_wall3/Grandcanyon.jpg",
+  imgTitle:"Grand Canyon",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'20"x14" inches',
+  imgPrice:"$1,900.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var HoleInRock={
+  imgLoc:"HoleInRock",
+  imgSrc:"images/thedesertandgreatbasin_wall3/Holeinrock.jpg",
+  imgTitle:"Hole in rock",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'13"x22" inches',
+  imgPrice:"$2,100.",
+  origIsAvail:true,
+  imgPrtPrice:"$0,000.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var NearLonePine={
+  imgLoc:"NearLonePine",
+  imgSrc:"images/thedesertandgreatbasin_wall3/Nearlonepine.jpg",
+  imgTitle:"Near Lone Pine",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'24"x40" inches',
+  imgPrice:"$4,700.",
+  origIsAvail:true,
+  imgPrtPrice:"",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var PalmOasis={
+  imgLoc:"PalmOasis",
+  imgSrc:"images/thedesertandgreatbasin_wall3/Palmoasis.jpg",
+  imgTitle:"Palm Oasis",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'15"x10" inches',
+  imgPrice:"$750.",
+  origIsAvail:true,
+  imgPrtPrice:"",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var SandVerbinaNearPalmSprings={
+  imgLoc:"SandVerbinaNearPalmSprings",
+  imgSrc:"images/thedesertandgreatbasin_wall3/Sanverbi.jpg",
+  imgTitle:"Sand Verbina near Palm Springs",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'22"x48" inches`',
+  imgPrice:"$10,800.",
+  origIsAvail:true,
+  imgPrtPrice:"",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var wall_3=[AlabamaHills,GrandCanyon,HoleInRock,NearLonePine,PalmOasis,SandVerbinaNearPalmSprings]
+//end wall Three
+
+// Wall four
+var MtBurdell={
+  imgLoc:"MtBurdell",
+  imgSrc:"images/thesierraandmtpeaks_wall4/smmtburdellprint.jpg",
+  imgTitle:"Mt. Burdell",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'22"x48" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$1,400.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var WhiteMountain={
+  imgLoc:"WhiteMountain",
+  imgSrc:"images/thesierraandmtpeaks_wall4/whitemtn.jpg",
+  imgTitle:"White Mountain",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'20"x40" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$900.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var RocksInYosemite={
+  imgLoc:"RocksInYosemite",
+  imgSrc:"images/thesierraandmtpeaks_wall4/rocksinyosemite.jpg",
+  imgTitle:"Rocks in Yosemite",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'20"x14" inches',
+  imgPrice:"$1,900.",
+  origIsAvail:true,
+  imgPrtPrice:"",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var MtDiablofromShortRidge={
+  imgLoc:"MtDiablofromShortRidge",
+  imgSrc:"images/thesierraandmtpeaks_wall4/pmtdiablo.jpg",
+  imgTitle:"Mt. Diablo from Short Ridge",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'18"x36" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$1,000.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var IcePatch={
+  imgLoc:"IcePatch",
+  imgSrc:"images/thesierraandmtpeaks_wall4/Icepatch.jpg",
+  imgTitle:"Ice Patch",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'15"x10" inches',
+  imgPrice:"$650.",
+  origIsAvail:true,
+  imgPrtPrice:"",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var BlackMountain={
+  imgLoc:"BlackMountain",
+  imgSrc:"images/thesierraandmtpeaks_wall4/cblackmtn.jpg",
+  imgTitle:"Black Mountain",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'24"x48" inches',
+  imgPrice:"$9,800.",
+  origIsAvail:true,
+  imgPrtPrice:"$1,400.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var wall_4=[MtBurdell,WhiteMountain,RocksInYosemite,MtDiablofromShortRidge,IcePatch,BlackMountain];
+
+//end wall four
+
+// Wall five
+var Tracks={
+  imgLoc:"Tracks",
+  imgSrc:"images/humanartifacts_wall5/framedprinttracks.jpg",
+  imgTitle:"Tracks",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'16"x26" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$900.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var Spsd9e={
+  imgLoc:"Spsd9e",
+  imgSrc:"images/humanartifacts_wall5/SD9E-Train.jpg",
+  imgTitle:"SP SD9E's Petaluma, CA 1992",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'32"x48" inches',
+  imgPrice:"$12,500.",
+  origIsAvail:true,
+  imgPrtPrice:"$1,400.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var Spsw2595={
+  imgLoc:"Spsw2595",
+  imgSrc:"images/humanartifacts_wall5/ssw2595L.jpg",
+  imgTitle:"SP-SW2595 switcher",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'18"x32" inches',
+  imgPrice:"$7,800.",
+  origIsAvail:true,
+  imgPrtPrice:"$900.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var OldWarehouse={
+  imgLoc:"OldWarehouse",
+  imgSrc:"images/humanartifacts_wall5/framedprint.jpg",
+  imgTitle:"Old Warehouse",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'32"x48" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$1,400.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var TomalesNurseryTruck={
+  imgLoc:"TomalesNurseryTruck",
+  imgSrc:"images/humanartifacts_wall5/TomalesNurseryTruck.jpg",
+  imgTitle:"Tomales Nursery Truck",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'16"x24" inches and 12"x18" inches for smaller edition',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$900. and $400. for the smaller edition",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var FiftySevenFordTruck={
+  imgLoc:"FiftySevenFordTruck",
+  imgSrc:"images/humanartifacts_wall5/p57ford.jpg",
+  imgTitle:"'57 Ford Pickup",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'16"x24" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$800.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var ChevyPickup={
+  imgLoc:"ChevyPickup",
+  imgSrc:"images/humanartifacts_wall5/chevytruck.jpg",
+  imgTitle:"Chevy Pickup",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'16"x20" inches',
+  imgPrice:"$5,500.",
+  origIsAvail:true,
+  imgPrtPrice:"$00.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var PetalumaMill={
+  imgLoc:"PetalumaMill",
+  imgSrc:"images/humanartifacts_wall5/frprint-petmill.jpg",
+  imgTitle:"Petaluma Mill",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'14"x22" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$700.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var FiftySevenFordno2={
+  imgLoc:"FiftySevenFordno2",
+  imgSrc:"images/humanartifacts_wall5/57fordno2.jpg",
+  imgTitle:"'57 Ford #2",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'16"x24" inches',
+  imgPrice:"$5,500.",
+  origIsAvail:true,
+  imgPrtPrice:"$00.",
+  printIsAvail:false,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var FranksFirstTruck={
+  imgLoc:"FranksFirstTruck",
+  imgSrc:"images/humanartifacts_wall5/pfrankstruck.jpg",
+  imgTitle:"Frank's First Truck",
+  imgDesc:"img desc: It's a cool original.",
+  imgSize:'16"x24" inches',
+  imgPrice:"$00.",
+  origIsAvail:false,
+  imgPrtPrice:"$900.",
+  printIsAvail:true,
+  prtDesc:"The print is an actual size Giclee reproduction of the original painting.",
+}
+var wall_5=[Tracks,Spsd9e,Spsw2595,OldWarehouse,TomalesNurseryTruck,FiftySevenFordTruck,ChevyPickup,PetalumaMill,FiftySevenFordno2,FranksFirstTruck];
+
+//end wall five
+var walls=[wall_1,wall_2,wall_3,wall_4,wall_5]
